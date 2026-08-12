f5dfa2a0e895705662e987d6ca667fbde7067bd7
Wesley Barnett <amazonwes93@gmail.com>
2026-08-12 10:05:45 -0700

    Enemy

---
+++ Unity Prototype/Dev2-Prototype1/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float baseSpeed = 2f;
+    public float baseHealth = 100f;
+    public float baseDamage = 10f;
+
+    public float speed;
+    public float health;
+    public float damage;
+
+    public float speedIncrease = 0.25f;
+    public float healthIncrease = 20f;
+    public float damageIncrease = 5f;
+
+    private void ApplyLevelDifficulty()
+    {
+        int level = levelManager.currentLevel;
+
+        speed = baseSpeed + ((level - 1) * speedIncrease);
+        health = baseHealth + ((level - 1) * healthIncrease);
+        damage = baseDamage + ((level - 1) * damageIncrease);
+    }
+}
