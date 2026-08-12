4c5eafa49a9dc68dc5051c2a20ef186394c24fc0
Tyler Nelson <tylern247@gmail.com>
2026-08-12 13:40:41 -0500

    Camera
    Added cameraController

---
+++ Unity Prototype/Dev2-Prototype1/Assets/Scripts/cameraController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    [Header("Camera Settings")]
+    [Range(10, 200)][SerializeField] int Sense;
+    [SerializeField] int minVert, maxVert;
+
+    float camRotX;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameManager.instance.isPaused)
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X") * Sense * Time.deltaTime;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Sense * Time.deltaTime;
+
+            camRotX -= mouseY;
+            camRotX = Mathf.Clamp(camRotX, minVert, maxVert);
+            transform.localRotation = Quaternion.Euler(camRotX, 0, 0);
+
+            transform.parent.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
