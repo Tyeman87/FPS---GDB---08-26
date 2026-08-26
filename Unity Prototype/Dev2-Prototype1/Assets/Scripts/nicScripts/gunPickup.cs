0137dc3b414c11952a14621a5dc867d8ebb40ec2
epicinsanity98-max <epicinsanity98@gmail.com>
2026-08-25 23:12:45 -0400

    level changes

--- Unity Prototype/Dev2-Prototype1/Assets/Scripts/nicScripts/gunPickup.cs
+++ Unity Prototype/Dev2-Prototype1/Assets/Scripts/nicScripts/gunPickup.cs
@@ -3,6 +3,28 @@ using UnityEngine;
 public class gunPickup : MonoBehaviour
 {
     [SerializeField] gunStats gun;
+    [Range(0, 100)] [SerializeField] float roationSpeed = 100f;
+    [SerializeField] float bobHeight = 0.25f;
+    [SerializeField] float bobSpeed = 2f;
+
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0f, roationSpeed * Time.deltaTime, 0f);
+        float newY = startPosition.y + Mathf.Sin(Time.time * bobSpeed) * bobHeight;
+
+        transform.position = new Vector3(
+            startPosition.x,
+            newY,
+            startPosition.z
+        );
+    }
 
     private void OnTriggerEnter(Collider other)
     {
