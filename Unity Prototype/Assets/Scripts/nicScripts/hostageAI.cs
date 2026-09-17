54870527201d9e9f1d3fd287939adeba0f90566a
Wesley Barnett <amazonwes93@gmail.com>
2026-09-17 09:06:58 -0700

    modified hostageAI

--- Unity Prototype/Assets/Scripts/nicScripts/hostageAI.cs
+++ Unity Prototype/Assets/Scripts/nicScripts/hostageAI.cs
@@ -28,6 +28,10 @@ public class hostageAI : MonoBehaviour, IDamage, IInteractable
 
     private bool rescued = false;
     private bool followingPlayer = false;
+    private bool IsRescued()
+    {
+        return rescued;
+    }
 
     private void Start()
     {
